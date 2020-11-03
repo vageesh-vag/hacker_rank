317785bedef59e6624a2bbfa9874a47ea43a7721
Vageesh Padigela <vageesh.padigela@gmail.com>
2020-11-03 13:36:23 +0530

    Create Day of the Programmer.cpp

---
+++ implementation/Day of the Programmer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int year;
+    cin>>year;
+    if (year == 1918)
+    {
+        cout<<"26.09."<<year;
+        return 0;
+    }
+    if(((year<1918)&&(year%4==0))||((year>1918)&&((year%4==0)&&(year%100!=0))||(year%400==0)))
+    cout<<"12.09."<<year;
+    else
+    cout<<"13.09."<<year;
+
+}

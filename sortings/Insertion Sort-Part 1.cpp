a9ceba8813a4139cc7e02ec83a27823cc1346ee3
Vageesh Padigela <vageesh.padigela@gmail.com>
2020-11-26 12:45:12 +0530

    Create Insertion Sort-Part 1.cpp

---
+++ sortings/Insertion Sort-Part 1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long int n,a[10000],f,i,j;
+    cin>>n;
+    for(i=0;i<n;i++)
+    cin>>a[i];
+    //for(i=0;i<n;i++)
+    //cout<<a[i];
+    f=a[n-1];
+    for(i=n-2;i>=0;i--)
+    {
+        if(f>a[i])
+        {
+        a[i+1]=f;
+        for(j=0;j<n;j++)
+        cout<<a[j]<<" ";
+        cout<<"\n";
+        break;
+        }
+        else
+        {
+            a[i+1]=a[i];
+            for(j=0;j<n;j++)
+            cout<<a[j]<<" ";
+            cout<<"\n";
+        }
+    }
+    if(f<a[0])
+    {
+        a[0]=f;
+        for(i=0;i<n;i++)
+        cout<<a[i]<<" ";
+        cout<<"\n";
+    }
+}

73a831445e47945e818d6d13a6c5e577b1a866c8
Vageesh Padigela <vageesh.padigela@gmail.com>
2020-11-27 12:37:00 +0530

    Create Marc's Cakewalk.py

---
+++ Greedy/Marc's Cakewalk.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the marcsCakewalk function below.
+def marcsCakewalk(calorie):
+    calorie.sort(reverse=True)
+    f=0
+    for i in range(len(calorie)):
+        f=f+(calorie[i]*2**i)
+    return f
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

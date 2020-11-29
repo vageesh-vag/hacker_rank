e08204e179d48b6ddc023105d28684cd615fa842
Vageesh Padigela <vageesh.padigela@gmail.com>
2020-11-29 14:25:58 +0530

    Create Tower Breakers.py

---
+++ Game theory/Tower Breakers.py
@@ -0,0 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+def towerBreakers(n, m):
+    return 2 if m==1 or n%2==0 else 1
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    t = int(input())
+    for t_itr in range(t):
+        nm = input().split()
+        n = int(nm[0])
+        m = int(nm[1])
+        result = towerBreakers(n, m)
+        fptr.write(str(result) + '\n')
+    fptr.close()
